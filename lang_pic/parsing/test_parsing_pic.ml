0aedd2db273c807d2ed4d7a0ecbbdcbbcc3173bf
nakyau <nakyau@gmail.com>
2012-11-23 13:12:44 +0900

    add -parse_pic option

--- lang_pic/parsing/test_parsing_pic.ml
+++ lang_pic/parsing/test_parsing_pic.ml
@@ -21,6 +21,31 @@ let test_tokens_pic file =
   toks +> List.iter (fun x -> pr2_gen x);
   ()
 
+
+let test_parse_pic xs  =
+  let ext = ".*\\.\\(pic\\|javascript\\)$" in
+
+  (* could now use Lib_parsing_php.find_php_files_of_dir_or_files *)
+  let fullxs = Common.files_of_dir_or_files_no_vcs_post_filter ext xs in
+
+  let stat_list = ref [] in
+
+  Common.check_stack_nbfiles (List.length fullxs);
+
+  fullxs +> List.iter (fun file -> 
+    pr2 ("PARSING: " ^ file);
+
+    if file =~ ".*/third_party" || file =~ ".*/wiki/extensions"
+    then pr2 "IGNORING third party directory, bad unicode chars"
+    else begin
+
+      let (xs, stat) = Parse_pic.parse file in
+      Common.push2 stat stat_list;
+    end
+  );
+  Parse_info.print_parsing_stat_list !stat_list;
+  ()
+
 (*****************************************************************************)
 (* Unit tests *)
 (*****************************************************************************)
@@ -32,4 +57,8 @@ let test_tokens_pic file =
 let actions () = [
   "-tokens_pic", "   <file>", 
   Common.mk_action_1_arg test_tokens_pic;
+
+  "-parse_pic", "   <file>", 
+  Common.mk_action_n_arg test_parse_pic;
+
 ]
